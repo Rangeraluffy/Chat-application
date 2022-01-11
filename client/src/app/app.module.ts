d0a5f3248bd7d481e8f938b6ab4eb7186fc9d79b
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 15:04:20 +0100

    create component user name

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './containers/chat/chat.component';
+import { UsernameComponent } from './components/username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule
