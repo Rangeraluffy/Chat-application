490ffae51878e2b00d9fa64c61e9fb0825cbb3e7
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 15:14:36 +0100

    setUserName and html components

--- client/src/app/components/username/username.component.ts
+++ client/src/app/components/username/username.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-username',
   templateUrl: './username.component.html',
   styleUrls: ['./username.component.scss']
 })
-export class UsernameComponent implements OnInit {
+export class UsernameComponent{
+
+  userName = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  setUserName() {
+
   }
 
 }
