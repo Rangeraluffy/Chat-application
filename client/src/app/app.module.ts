74df6929a1a6f0810a5246bdff25415522b5d040
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 16:14:34 +0100

    loop msg and userlist in chat component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { ChatComponent } from './containers/chat/chat.component';
 import { UsernameComponent } from './components/username/username.component';
