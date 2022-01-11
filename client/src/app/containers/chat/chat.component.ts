727c18da8cdc022627a06dbdc2cbbb032b37e45e
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 15:54:04 +0100

    Chat components function sendMessage

--- client/src/app/containers/chat/chat.component.ts
+++ client/src/app/containers/chat/chat.component.ts
@@ -17,8 +17,26 @@ export class ChatComponent{
   constructor() { }
 
   userNameUpdate(name: string): void {
-    this.socket = io.io(`localhost:3000?userName=${name}`)
+    this.socket = io.io(`localhost:3000?userName=${name}`);
     this.userName = name;
+
+    this.socket.emit('set-user-name', name);
+    
+    this.socket.on('user-list', (userList: string[]) => {
+      this.userList = userList;
+    });
+
+    this.socket.on('message-broadcast', (data: {message: string, userName: string}) => {
+      if (data) {
+        this.messageList.push({message: data.message, userName: data.userName, mine: false});
+      }
+    });
   }
 
+  sendMessage(): void {
+    this.socket.emit('message', this.message);
+    this.messageList.push({message: this.message, userName: this.userName, mine: true});
+    this.message = '';
+  }
+  
 }
