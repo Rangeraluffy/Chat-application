befe4bcc291aee200bff31d8f8ae1f2198f56cf3
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 14:22:31 +0100

    Set up server js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+
+http.listen(3000, () => {
+    console.log('Server is running');
+})
