4ab0532076f95f2499905e95631b713b1099614e
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-11 14:24:04 +0100

    Create userList map and function Add User

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 })
 
+let userList = new Map();
+
+io.on('connection', (socket) => {
+
+});
+
+function addUser(userName, id) {
+    if (!userList.has(userName)) {
+        userList.set(userName, new Set(id));
+    } else {
+        userList.get(userName).add(id);
+    }
+}
 
 http.listen(3000, () => {
     console.log('Server is running');
